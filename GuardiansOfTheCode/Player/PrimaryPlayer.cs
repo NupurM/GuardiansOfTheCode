a3c5e34f128d25f78428f0477f14048db95fcb9e
NM <NM>
2018-09-11 20:18:30 -0400

    Creational Pattern:  Singleton - Create primary player

---
+++ GuardiansOfTheCode/Player/PrimaryPlayer.cs
@@ -0,0 +1,25 @@
+﻿namespace GuardiansOfTheCode.Player
+{
+    public sealed class PrimaryPlayer
+    {
+        public static PrimaryPlayer Instance { get; }
+        private PrimaryPlayer() { }
+
+        /// <summary>
+        /// A static  constructor is called only before
+        /// the first initialization of a class.
+        /// This is perfect for a singleton.
+        /// </summary>
+        static PrimaryPlayer()
+        {
+            Instance = new PrimaryPlayer()
+            {
+                Name = "Raptor",
+                Level = 1
+            };
+        }
+
+        public string Name { get; set; }
+        public int Level { get; set; }
+    }
+}
--- GuardiansOfTheCode/Program.cs
+++ GuardiansOfTheCode/Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GuardiansOfTheCode.Player;
+using System;
 
 namespace GuardiansOfTheCode
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var player = PrimaryPlayer.Instance;
+            Console.WriteLine($"Player: {player.Name} - Level {player.Level} ");
+
+            Console.ReadKey();
         }
     }
 }
